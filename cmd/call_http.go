a04127d2f1cbe76067f04e9ee1c16b84660e6c6f
Jumpaku <Jumpaku@users.noreply.github.com>
2023-03-04 11:57:09 +0000

    fix: http method

--- cmd/call_http.go
+++ cmd/call_http.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"os"
+	"strings"
 
 	"github.com/Jumpaku/api-regression-detector/lib/call/http"
 	libcmd "github.com/Jumpaku/api-regression-detector/lib/cmd"
@@ -13,6 +14,22 @@ import (
 
 func RunCallHTTP(stdio *cli.Stdio, endpointURL string, method http.Method, headers []string) (code int) {
 	errorInfo := errors.Info{"endpointURL": endpointURL, "method": method, "headers": headers}
+
+	method = http.Method(strings.ToUpper(string(method)))
+	switch method {
+	default:
+		PrintError(os.Stderr, errors.BadArgs.New(errorInfo.AppendTo("fail RunCallHTTP")))
+		return 1
+	case http.MethodGet,
+		http.MethodHead,
+		http.MethodPost,
+		http.MethodPut,
+		http.MethodPatch,
+		http.MethodDelete,
+		http.MethodConnect,
+		http.MethodOptions,
+		http.MethodTrace:
+	}
 
 	headerMap := map[string][]string{}
 	for _, header := range headers {
